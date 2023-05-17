876aba7b6eb69811817062201121285e2ff6adbe
Anit Jha <anitkjha31@gmail.com>
2023-05-18 01:50:36 +0530

    doc: updated readme

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 import Chessboard from "@/lib/Chessboard";
 import { getKingPositions } from "@/lib/GetKingSquare";
 import { Chess } from "chess.js";
+
 export default function Home() {
   const [fen, setFen] = useState<string | null>(null);
 
@@ -12,7 +13,7 @@ export default function Home() {
   return (
     <main className="min-h-[calc(100dvh_-_64px)] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black pt-8">
       <Chessboard
-        onDrop={(move) => {
+        onDrop={(move: any) => {
           try {
             const MOVE = game.current.moves({ verbose: true });
             const moves = game.current.move({
