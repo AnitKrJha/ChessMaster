21b9374f55e68a5bf3e55a3f9f4a7a63817f2dd0
Anit Jha <anitkjha31@gmail.com>
2023-05-23 02:01:11 +0530

    fixed next middleware

---
+++ src/app/play/[gid]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const PlayGame = (props: Props) => {
+  return <div>Coming Soon</div>;
+};
+
+export default PlayGame;
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,17 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareSupabaseClient({ req, res });
-  await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (
+    !session &&
+    (req.nextUrl.pathname.startsWith("/dashboard") ||
+      req.nextUrl.pathname.startsWith("/play"))
+  ) {
+    return NextResponse.rewrite(new URL("/login", req.url));
+  }
+
   return res;
 }
